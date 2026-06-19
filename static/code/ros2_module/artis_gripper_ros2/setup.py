a2c2bde1b07a6edd42e41fabc0a5d9c19e01b90e
RomanMykhailyshyn <mykhailyshyn@tntu.edu.ua>
2026-06-19 11:56:16 +0900

    Update setup.py

--- static/code/ros2_module/artis_gripper_ros2/setup.py
+++ static/code/ros2_module/artis_gripper_ros2/setup.py
@@ -10,16 +10,13 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools", "artis-gripper"],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="ARTiS developers",
+    maintainer="ARTiS Team",
     maintainer_email="user@example.com",
-    description="ROS 2 node for controlling the ARTiS gripper",
+    description="ROS 2 ARTiS gripper node",
     license="MIT",
     entry_points={
-        "console_scripts": [
-            "artis_node = artis_gripper_ros2.artis_node:main",
-            "preset_client = artis_gripper_ros2.preset_client:main",
-        ],
+        "console_scripts": ["artis_node = artis_gripper_ros2.artis_node:main"],
     },
 )
