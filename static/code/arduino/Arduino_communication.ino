8da0cc2279eccabb4014b46cd4897fa995df0eda
RomanMykhailyshyn <mykhailyshyn@tntu.edu.ua>
2026-06-19 11:55:29 +0900

    Update Arduino_communication.ino

--- static/code/arduino/Arduino_communication.ino
+++ static/code/arduino/Arduino_communication.ino
@@ -1,16 +1,12 @@
-// ARTiS jamming-palm relay controller
-// Serial protocol:
-//   '1' -> jamming ON  / relay energized
-//   '0' -> jamming OFF / relay released
-//   '?' -> print current state
+// ARTiS jamming-palm relay firmware.
+// Serial protocol: '1' = jam ON, '0' = jam OFF, '?' = print current state.
 
-#define RELAY_PIN 14
-#define RELAY_ACTIVE_LOW true
+const int RELAY_PIN = 14;
+const bool RELAY_ACTIVE_LOW = true;  // set false if your relay turns ON with HIGH
+bool jammed = false;
 
-bool jamming_on = false;
-
-void writeRelay(bool on) {
-  jamming_on = on;
+void setJamming(bool on) {
+  jammed = on;
   if (RELAY_ACTIVE_LOW) {
     digitalWrite(RELAY_PIN, on ? LOW : HIGH);
   } else {
@@ -21,21 +17,21 @@ void writeRelay(bool on) {
 void setup() {
   Serial.begin(9600);
   pinMode(RELAY_PIN, OUTPUT);
-  writeRelay(false);
-  Serial.println("ARTiS palm relay ready. 1=ON, 0=OFF, ?=STATE");
+  setJamming(false);
+  Serial.println("ARTiS palm ready: 1=jam_on, 0=jam_off");
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
     if (command == '1') {
-      writeRelay(true);
-      Serial.println("JAMMING_ON");
+      setJamming(true);
+      Serial.println("JAM_ON");
     } else if (command == '0') {
-      writeRelay(false);
-      Serial.println("JAMMING_OFF");
+      setJamming(false);
+      Serial.println("JAM_OFF");
     } else if (command == '?') {
-      Serial.println(jamming_on ? "JAMMING_ON" : "JAMMING_OFF");
+      Serial.println(jammed ? "JAM_ON" : "JAM_OFF");
     }
   }
 }
